aec8c3a078815b4038ad566def0eb42e76c9b55a
Brian Burkard <brianburkard@gmail.com>
2022-11-26 13:04:40 -0500

    feat:  - Using supabase Auth component again  - More server side validation on routes  - Index page only used for login, then routes to profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,6 +2,11 @@ import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs"
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react"
 import { useProfileData } from "../hooks/useProfileData"
 import Layout from '../components/Layout';
+import { Input, Button } from "reakit";
+import { Spinner } from "../components/Spinner";
+
+import getProfile from '../utils/get-profile';
+import { useState } from "react";
 
 
 export const getServerSideProps = async (ctx) => {
@@ -9,10 +14,10 @@ export const getServerSideProps = async (ctx) => {
   const supabase = createServerSupabaseClient(ctx)
   // Check if we have a session
   const {
-    data: { session },
-  } = await supabase.auth.getSession()
+    data: { user },
+  } = await supabase.auth.getUser()
 
-  if (!session)
+  if (!user)
     return {
       redirect: {
         destination: '/',
@@ -20,26 +25,81 @@ export const getServerSideProps = async (ctx) => {
       },
     }
 
+  const userProfile = await getProfile(supabase, user.id)
+
+  console.log('userProfile', userProfile)
+  // check if user is in users (profile) table.
+
+  // new user route
+  if (!userProfile) {
+    return {
+      props: {
+        newUser: true,
+        user: null,
+      }
+    }
+  }
+
+
   return {
     props: {
+      newUser: false,
     },
   }
 }
 
 
-const ProfilePage = ({ }) => {
+const ProfilePage = ({ newUser }) => {
 
-  const [loading, displayName, profileId, error] = useProfileData();
+  const [username, setUsername] = useState("");
+
+  if (newUser) {
+    return (
+      <Layout title="Profile">
+        <div className="container" style={{ padding: '20px 0 100px 0' }}>
+            <h2>Welcome New User!</h2>
+            <p>Fill out some info below to get started</p>
+          <form action="/api/adduser" method="post" className="flex flex-col w-60 gap-2 mt-12">
+            <label className="text-left" htmlFor="email">Desired Username:</label>
+            <Input required className="border py-1" placeholder="username" id="username" name="username" onChange={(e => setUsername(e.target.value))} value={username} />
+            <Button
+              className="p-2 bg-purple-500 text-white rounded-md"
+              type="submit"
+              value="Submit"
+            // onClick={}
+            >
+              Let's Go!
+            </Button>
+          </form>
+        </div>
+      </Layout >
+    )
+  }
 
   return (
     <Layout title="Profile">
       <div className="container" style={{ padding: '20px 0 100px 0' }}>
-        <p className="text-center text-xl">Welcome {displayName}!!</p>
-        TODO: Add a profile picture here.
-
+        Welcome Returning User
       </div>
-    </Layout>
+    </Layout >
   )
+  // const [loading, displayName, profileId, error] = useProfileData();
+
+  // console.log('loading', loading);
+  // console.log('display name', displayName);
+
+  // return (
+  //   <Layout title="Profile">
+  //     {loading && !displayName ? (
+  //       <Spinner />
+  //     ) : displayName ? (
+  //       <div className="container" style={{ padding: '20px 0 100px 0' }}>
+  //         <p className="text-center text-xl">Welcome {displayName}!!</p>
+  //       </div>
+  //     ) : null
+  //     }
+  //   </Layout >
+  // )
 }
 
 export default ProfilePage;
